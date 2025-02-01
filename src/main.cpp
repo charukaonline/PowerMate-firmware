3a96004e20e84eb4ccd8c0bb20c53d1794903847
charukaonline <nuwanthakonline@gmail.com>
2025-02-01 13:47:58 +0530

    ultrasonic (realtime distance)

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,58 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
-// Define the network credentials
-const char *ssid = "ESP32_Hotspot";
-const char *password = "password123";
+#define echoPin 2  
+#define trigPin 4     
+const char* ssid = "CNK2";
+const char* password = "fibre2403";
+const char* serverUrl = "http://192.168.1.37:5000/api/distance";
 
-void setup() {
-  // Start Serial Monitor for debugging
+long duration;
+float distance;
+
+void setup(){
   Serial.begin(115200);
-  Serial.println("Starting Access Point...");
+  WiFi.begin(ssid, password);
 
-  // Start the Wi-Fi as an access point
-  WiFi.softAP(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 
-  // Print the IP address
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("Access Point IP Address: ");
-  Serial.println(IP);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
-void loop() {
-  // Blink the onboard LED to indicate the ESP32 is running
-  const int ledPin = 2;
-  pinMode(ledPin, OUTPUT);
+void loop(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
   
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58.2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonPayload = "{\"value\":" + String(distance) + "}";
+    int httpResponseCode = http.POST(jsonPayload);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(5000);
 }

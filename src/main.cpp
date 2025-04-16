e76605387ec9ddc0d20212039ff7054b3ecb87f3
charukaonline <nuwanthakonline@gmail.com>
2025-04-16 15:13:43 +0530

    update code to get temp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+// WiFi credentials
+const char *ssid = "CNK2";
+const char *password = "fibre2403";
+
+// Server URL - simplified
+const char *serverUrlSensorData = "http://192.168.1.37:5000/api/sensor-data/temp-data";
 
 // Data wire is connected to GPIO4 (can be changed)
 #define ONE_WIRE_BUS 14
@@ -12,6 +22,67 @@ void setup()
 {
     Serial.begin(115200);
     sensors.begin();
+
+    // Connect to WiFi
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void sendTemperatureData(float temperature)
+{
+    // Check WiFi connection status
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+
+        // Specify the URL
+        http.begin(serverUrlSensorData);
+
+        // Specify content-type header
+        http.addHeader("Content-Type", "application/json");
+
+        // Create JSON document
+        DynamicJsonDocument doc(200);
+        doc["temperature"] = temperature;
+
+        // Serialize JSON document
+        String json;
+        serializeJson(doc, json);
+
+        // Send HTTP POST request
+        int httpResponseCode = http.POST(json);
+
+        // Check response
+        if (httpResponseCode > 0)
+        {
+            String response = http.getString();
+            Serial.println("HTTP Response code: " + String(httpResponseCode));
+            Serial.println("Response: " + response);
+        }
+        else
+        {
+            Serial.print("Error on sending POST: ");
+            Serial.println(httpResponseCode);
+        }
+
+        // Free resources
+        http.end();
+    }
+    else
+    {
+        Serial.println("WiFi Disconnected");
+    }
 }
 
 void loop()
@@ -23,5 +94,8 @@ void loop()
     Serial.print(tempC);
     Serial.println(" °C");
 
-    delay(1000);
+    // Send temperature data to API
+    sendTemperatureData(tempC);
+
+    delay(5000); // Send data every 5 seconds
 }
